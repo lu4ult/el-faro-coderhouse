c9856c4c4e69dd19b154d7489305e7bc61bce28c
Lautaro <lu4ult@gmail.com>
2022-09-09 19:11:12 -0300

    programa 2022-09-08

--- pages/emisiones/elfaro-emisiones-generador.py
+++ pages/emisiones/elfaro-emisiones-generador.py
@@ -103,10 +103,10 @@ with open(".\pages\emisiones\wa-link.html", 'w') as f:
     #print(descripciones[episodios[0]][1])
     texto = "Ya est%C3%A1 disponible el *programa "+descripciones[episodios[0]][0]+" del "+episodios[0]+" de El Faro!*%0a%0a%0a"
     texto += descripciones[episodios[0]][1]
-    texto += "%0a%0a%0aPodes esucharla en el siguiente link:%0ahttps://lu4ult.com/el-faro/emisiones/emisiones.html%0a%0a"
+    texto += "%0a%0a%0aPodes esucharla en el siguiente link:%0ahttps://lu4ult.github.io/el-faro-coderhouse/pages/emisiones/emisiones.html%0a%0a"
     texto += "O en spotify:%0ahttps://open.spotify.com/show/26ZvU92WzcChhUnhaSxzPw"
 
-    f.write("<a target=\"__blank\" href=\"https://wa.me/542954543687?text="+texto+"\">Link Fernando</a>")
+    f.write("<a target=\"__blank\" href=\"https://wa.me/542954543687?text="+texto+"\">Link Fernando</a><br>")
     f.write("<a target=\"__blank\" href=\"https://wa.me/542954692293?text="+texto+"\">Link Lautaro</a>")
     f.write("</body>\n</html>")
     f.close()
